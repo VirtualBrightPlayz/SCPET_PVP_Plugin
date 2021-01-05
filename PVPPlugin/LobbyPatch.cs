ab5a43b72f3b5bc8c7ae93fccbfedc8fe353d2e5
VirtualBrightPlayz <lego.trains.b@gmail.com>
2021-01-04 18:04:32 -0700

    added map randomizer

--- PVPPlugin/LobbyPatch.cs
+++ PVPPlugin/LobbyPatch.cs
@@ -1,11 +1,16 @@
-﻿using HarmonyLib;
+﻿using Assets.MirrorEXT;
+using ExamplePlugin;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using VirtualBrightPlayz.SCP_ET;
 using VirtualBrightPlayz.SCP_ET.Network;
+using VirtualBrightPlayz.SCP_ET.Player;
 
 namespace PVPPlugin
 {
@@ -14,15 +19,23 @@ namespace PVPPlugin
     {
         public static void Postfix(CustomLobby __instance)
         {
-            __instance.StartGame();
+            //__instance.StartGame();
+            string map = MainClass.config.Maps[UnityEngine.Random.Range(0, MainClass.config.Maps.Length)];
+            CustomNetworkManager.customMapJson = File.ReadAllText(map);
+            CustomNetworkManager.mapName = Path.GetFileNameWithoutExtension(map);
         }
     }
 
-    [HarmonyPatch(typeof(RoundEnd), nameof(RoundEnd.EndRound))]
+    [HarmonyPatch(typeof(RoundEnd), nameof(RoundEnd.Update))]
     public static class RoundEndPatch
     {
         public static bool Prefix(RoundEnd __instance)
         {
+            if (GameObject.FindObjectsOfType<PlayerController>().Length > 1)
+                return false;
+            if (__instance.roundEnded)
+                return false;
+            __instance.EndRound("ROUNDENDDIED", 15f);
             return false;
         }
     }
--- PVPPlugin/MainClass.cs
+++ PVPPlugin/MainClass.cs
@@ -1,10 +1,13 @@
 ﻿using HarmonyLib;
+using Newtonsoft.Json;
 using PluginFramework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using VirtualBrightPlayz.SCP_ET.ServerConsole;
 
 namespace ExamplePlugin
@@ -12,6 +15,12 @@ namespace ExamplePlugin
     public class MainClass : Plugin
     {
         public static Harmony inst;
+        public static PVPConfig config;
+
+        public class PVPConfig
+        {
+            public string[] Maps { get; set; }
+        }
 
         public override void OnDisable()
         {
@@ -24,6 +33,11 @@ namespace ExamplePlugin
             PluginSystem.Manager.Logger.Info("PVPPlugin", "INIT");
             inst = new Harmony("pvpplugin");
             inst.PatchAll();
+            string path = Path.Combine(Application.dataPath, "../settings/pvpconfig.json");
+            if (!File.Exists(path))
+                File.WriteAllText(path, JsonConvert.SerializeObject(new PVPConfig()));
+            string text = File.ReadAllText(path);
+            config = JsonConvert.DeserializeObject<PVPConfig>(text);
         }
     }
 }
